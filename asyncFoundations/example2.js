4bfd3798078736baba09d683e14937f18827d66d
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-04-12 10:21:58 -0700

    definitions for callback and higher order functions

--- asyncFoundations/example2.js
+++ asyncFoundations/example2.js
@@ -4,7 +4,10 @@ function sendMessage(message, callback) {
 }
 
 sendMessage("Message for console", console.log);
+//prints to console
 
 sendMessage("Message for alert", alert);
+//should create alert message (didn't work in chrome developer console)
 
 var answer = sendMessage("Are you sure?", confirm);
+//alert "from embedded page" message on webpage requiring button okay button click
