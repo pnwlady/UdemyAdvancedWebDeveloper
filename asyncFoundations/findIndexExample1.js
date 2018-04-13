7b836a4078741a63b5e3dcdd8d4d380588eeac43
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-04-13 10:09:56 -0700

    basic find index

--- asyncFoundations/findIndexExample1.js
+++ asyncFoundations/findIndexExample1.js
@@ -5,3 +5,14 @@ function findIndex(array, callback) {
 function callback(curElement, curIndex, array) {
     //callback implemented by caller of function
 }
+
+var arr = [3,4,6,2,1];
+findIndex(arr, function(num, index, array) {
+    return num === 6;
+});
+//this value will be truthy when the index value is equal to 6
+
+var arr = [5,11,13,8,6,7];
+findIndex(arr, function(num, index, array) {
+    return num % 2 === 0;
+});
