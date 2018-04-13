4441fba6d77639b3378ca7dd1fb857b3018c4dd3
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-04-13 10:04:50 -0700

    breakdown of function

---
+++ asyncFoundations/findIndexExample1.js
@@ -0,0 +1,7 @@
+function findIndex(array, callback) {
+    //find index code to be implemented
+}
+
+function callback(curElement, curIndex, array) {
+    //callback implemented by caller of function
+}
