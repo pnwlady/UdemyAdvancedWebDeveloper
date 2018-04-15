87860cf6f7622c625fdad8f9d41f4e7ca993f342
heidilaursen <heidilaursen@protonmail.com>
2018-04-15 16:27:04 -0700

    attempt 2

--- asyncFoundations/findIndexExmple2.js
+++ asyncFoundations/findIndexExmple2.js
@@ -1,20 +1,19 @@
 //Quiz: make findIndex
 function findIndex(array, callback) {
-    //if there is an index == truthy, if answer is -1 == then return falsey
-    if (curIndex) {
-        return truthy;
-    } else if (-1) {
-        return falsey;
-    }
-}
-
-function callback(curElement, curIndex, array) {
-    //iterate through array until curElement is found, return curIndex, or -1
     for(var i = 0; i < array.length; i++) {
-        if (array[i] === curElement) {
-            return curIndex;
+        if (callback(array[i], i, array)) {
+            return i;
         } else {
             return -1;
         }
     }
+}
+
+function findIndex(arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      return i;
+    }
+  }
+  return -1;
 }
