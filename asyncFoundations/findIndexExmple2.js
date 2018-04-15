4122c90ea2cb83e5db3c09335fbe598e0cf4d0e8
heidilaursen <heidilaursen@protonmail.com>
2018-04-15 16:27:42 -0700

    final answer - passing

--- asyncFoundations/findIndexExmple2.js
+++ asyncFoundations/findIndexExmple2.js
@@ -1,14 +1,4 @@
 //Quiz: make findIndex
-function findIndex(array, callback) {
-    for(var i = 0; i < array.length; i++) {
-        if (callback(array[i], i, array)) {
-            return i;
-        } else {
-            return -1;
-        }
-    }
-}
-
 function findIndex(arr, callback) {
   for (var i = 0; i < arr.length; i++) {
     if (callback(arr[i], i, arr)) {
