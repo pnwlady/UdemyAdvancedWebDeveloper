50c1b26311b3c94f43aa2bba2526e063a4d2cdcf
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-04-17 14:46:47 -0700

    setInterval and clearInterval

--- asyncFoundations/setIntervalExample.js
+++ asyncFoundations/setIntervalExample.js
@@ -3,3 +3,16 @@ setInterval(function() {
     num++;
     console.log("num:", num);
 }, 1000);
+
+//logs num: 1 and continues until we stop.
+
+var num = 0;
+var intervalId = setInterval(function() {
+    num++;
+    console.log("num:", num);
+    if (num === 3) {
+        clearInterval(intervalId);
+    }
+}, 1000);
+
+//logs num: 1 and continues until 3, stops after 3.
