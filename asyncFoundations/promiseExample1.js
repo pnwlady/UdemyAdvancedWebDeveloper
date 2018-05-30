54841223ce97927f1790c4bab8ff5d0d9820ebf1
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-05-30 11:10:16 -0700

    added notes

--- asyncFoundations/promiseExample1.js
+++ asyncFoundations/promiseExample1.js
@@ -1,7 +1,8 @@
+// p1 is promise object
 var p1 = new Promise(function(resolve, reject) {
     resolve([1,2,3,4]);
 });
-
+//handling resolve
 p1.then(function(arr) {
     console.log("Promise p1 resolved with data:", arr);
 });
